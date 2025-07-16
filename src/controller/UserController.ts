f4820686de79d1c3e1f9455a427a93d923b7b448
HulitosCode <heltonmatimbe8@gmail.com>
2025-07-16 11:41:30 +0200

    primeiro commit

---
+++ src/controller/UserController.ts
@@ -0,0 +1,15 @@
+import type { Request, Response } from "express"
+import { prisma } from "../database/prisma"
+
+export const createUser = async (req: Request, res: Response) => {
+    const { name, email, password } = req.body
+
+    const user = await prisma.user.create({
+        data: {
+            name,
+            email,
+            password
+        },
+    })
+    return res.json(user)
+}
