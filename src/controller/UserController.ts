7355485d1e2beeccae8ad9a5875e9046c30ace2c
HulitosCode <heltonmatimbe8@gmail.com>
2025-07-17 10:39:41 +0200

    feat: criando crud completo, e validar email e nivel de accesso do usuario. e retornar name, email e nivel de acesso usando select

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,15 +1,61 @@
-import type { Request, Response } from "express"
-import { prisma } from "../database/prisma"
+import type { Request, Response } from "express";
+import { prisma } from "../database/prisma";
+import { hash } from "bcryptjs";
 
 export const createUser = async (req: Request, res: Response) => {
-    const { name, email, password } = req.body
+  const { name, email, password, accessName } = req.body;
 
-    const user = await prisma.user.create({
-        data: {
-            name,
-            email,
-            password
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  const isAccessName = await prisma.access.findUnique({
+    where: {
+      name: accessName,
+    },
+  });
+
+  if (!isAccessName) {
+    return res.status(400).json({ message: "Acesso não encontrado!" });
+  }
+
+  if (existingUser) {
+    return res
+      .status(400)
+      .json({ message: "Usuario com esse email já existe!" });
+  }
+
+  const hashPassword = await hash(password, 10);
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashPassword,
+      Access: {
+        connectOrCreate: {
+          where: { name: accessName },
+          create: { name: accessName },
         },
-    })
-    return res.json(user)
-}
+      },
+    },
+    select: {
+      name: true,
+      email: true,
+      Access: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+  return res.json(user);
+};
+
+export const deleteManyUsers = async (req: Request, res: Response) => {
+  await prisma.user.deleteMany();
+
+  return res.json({ message: "Todos usuarios deletados com sucesso!" });
+};
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express'
-import { router } from './router';
+import express from "express";
+import { router } from "./router";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.use(router)
+app.use(router);
 
-app.listen(3333, () => console.log('Server is running on port 3333'));
+app.listen(3333, () => console.log("Server is running on port 3333"));
